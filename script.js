4bfdde5cc94b2fd83b79e8d06402fa4c1befe44f
Ronitpatidar <ronitpatidar22@gmail.com>
2025-05-29 10:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.querySelector(".btn").addEventListener("click", function () {
+  const search = document.querySelector(".search");
+  const input = document.querySelector(".input");
+
+  // Add 'active' class to show input
+  search.classList.add("active");
+
+  // Focus the input
+  input.focus();
+});
+
